84bc5e7d0bea0e205b1c790b011cb10308363526
ZFooox <113981639+ZFooox@users.noreply.github.com>
2024-10-22 12:29:11 +0200

    fix: how to get len to strlen

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jocroon <jocroon@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 11:29:46 by jocroon           #+#    #+#             */
-/*   Updated: 2024/10/21 14:59:54 by jocroon          ###   ########.fr       */
+/*   Updated: 2024/10/22 11:59:32 by jocroon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size)
 	size_t	src_len;
 
 	i = 0;
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
+	src_len = ft_strlen(src);
 	if (dest_size == 0)
 		return (src_len);
 	while (i < dest_size - 1 && src[i])
