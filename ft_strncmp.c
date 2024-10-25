a0973f034d163e72c881ab53684baf885589ccf2
ZFooox <113981639+ZFooox@users.noreply.github.com>
2024-10-25 16:37:25 +0200

    change strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jocroon <jocroon@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:22:16 by jocroon           #+#    #+#             */
-/*   Updated: 2024/10/24 16:15:30 by jocroon          ###   ########.fr       */
+/*   Updated: 2024/10/25 16:35:22 by jocroon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int	ft_strncmp(const char *str1, const char *str2, size_t len)
 	size_t	i;
 
 	i = 0;
-	while (i < len)
-	{
-		if (str1[i] != str2[i] || str1[i] == '\0' || str2[i] == '\0')
-			return ((unsigned char) str1[i] - (unsigned char) str2[i]);
+	while (i < len && str1[i] && str1[i] == str2[i])
 		i++;
-	}
-	return (0);
+	if (i == len)
+		return (0);
+	return ((unsigned char) str1[i] - (unsigned char) str2[i]);
 }
 /*
 #include <stdio.h>
