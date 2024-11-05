11c195eeb8205d52322c8afc64092f878f0255bf
ZFooox <113981639+ZFooox@users.noreply.github.com>
2024-11-05 16:56:54 +0100

    change varaible name

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,27 +6,27 @@
 /*   By: jocroon <jocroon@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 09:16:36 by jocroon           #+#    #+#             */
-/*   Updated: 2024/10/28 12:40:49 by jocroon          ###   ########.fr       */
+/*   Updated: 2024/11/05 16:43:49 by jocroon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len_big)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len_haystack)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	if (!*little)
-		return ((char *) big);
-	while (big[i] && i < len_big)
+	if (!*needle)
+		return ((char *) haystack);
+	while (haystack[i] && i < len_haystack)
 	{
 		j = 0;
-		while (little[j] && (i + j) < len_big && big[i + j] == little[j])
+		while (needle[j] && (i + j) < len_haystack && haystack[i + j] == needle[j])
 		{
-			if (!little[j + 1])
-				return ((char *) &big[i]);
+			if (!needle[j + 1])
+				return ((char *) &haystack[i]);
 			j++;
 		}
 		i++;
