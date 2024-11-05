779b399ed43058a414fa0aa36b7a255a0aa1dd1c
ZFooox <113981639+ZFooox@users.noreply.github.com>
2024-11-05 16:35:11 +0100

    add main to test

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: jocroon <jocroon@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:22:59 by jocroon           #+#    #+#             */
-/*   Updated: 2024/10/22 17:59:46 by jocroon          ###   ########.fr       */
+/*   Updated: 2024/11/05 16:34:09 by jocroon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,13 @@ size_t	ft_strlen(const char *str)
 	}
 	return (i);
 }
+
+/* #include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "Hello World";
+	
+	printf("%zu\n", ft_strlen(str));
+	return (0);
+} */
